4770fc17cd25bf4b2758dc888f648a12cfdad518
John <elsporko@gmail.com>
2017-06-29 16:06:06 -0400

    Begin move orders. Does not work properly yet.

--- battleship/static/javascript/battleship.js
+++ battleship/static/javascript/battleship.js
@@ -16,10 +16,21 @@ r.addEventListener('click',
 	    return;
     }, false);
 
-// Set up grid
-let g = document.getElementById('myGrid');
-g.appendChild(grid.clickableGrid(10, 10, ships, fleet));
+let f=document.getElementById('setFleet');
+f.addEventListener('click', 
+    function(){
+        document.getElementById('setFleet').style.display='none';
+        document.getElementById('playerGrid').style.display='inline';
+	    playGame();
+	    return;
+    }, false);
 
+// Set up grid
+document.getElementById('myGrid').appendChild(grid.clickableGrid(10, 10, ships, fleet));
+
+// Set up drag/drop of moves
+//document.getElementById('playOrder').setAttribute('draggable','true');
+//player.playerOrderHandler();
 
 /* Set random fleet */
 ships.buildShips();
@@ -32,24 +43,31 @@ for (t in wholeFleet) {
 /* 
  * Mock game will be removed 
  */
-let m = document.getElementById('Megan');
+let m = document.getElementById('MeganReg');
 m.addEventListener('click', 
     function(){
         player.acceptReg('Megan', 1, grid, ships, fleet, player);
-        document.getElementById('Megan').style.display='none';
+        //m.style.display='none';
+        document.getElementById('MeganReg').style.display='none';
+	//document.getElementById(flow[currentFlow]).style.display='none';
+        //m.appendChild(document.createElement('p'));
     }, false);
 
-let ry = document.getElementById('Ryan');
+let ry = document.getElementById('RyanReg');
 ry.addEventListener('click', 
     function(){
-        player.acceptReg('Ryan', 1, grid, ships, fleet, player);
-        document.getElementById('Ryan').style.display='none';
+        player.acceptReg('Ryan', 2, grid, ships, fleet, player);
+        document.getElementById('RyanReg').style.display='none';
+        //let r=document.getElementById('Ryan').style.display='none';
+        //r.appendChild(document.createElement('p'));
     }, false);
 
-//player.acceptReg('Megan', 1, grid, ships, fleet, player);
-//player.acceptReg('Ryan', 2, grid, ships, fleet, player);
-
-/* Set confirm fleet */
+let tr = document.getElementById('TraceyReg');
+tr.addEventListener('click', 
+    function(){
+        player.acceptReg('Tracey', 2, grid, ships, fleet, player);
+        document.getElementById('TraceyReg').style.display='none';
+    }, false);
 
 /* Play game */
 /*
@@ -57,3 +75,14 @@ while (1) {
 	player.getTurn();
 }
 */
+
+
+function playGame(){
+	if (player.myTurn()){
+
+		//window.open('','attack', 'height=200,width=200,menubar=no,status=no,titlebar=no,toolbar=no', false );
+	}
+}
+
+
+
--- battleship/static/javascript/player.js
+++ battleship/static/javascript/player.js
@@ -3,7 +3,8 @@ let fleet = require('./fleet.js');
 
 let playerRoster = new Object; // Placeholder for all players in the game
 let playerOrder = []; // Order of player turn
-//let playerMove = [];
+let playerMove = [];
+let playerMoveMap = {};
 
 let me;
 let orderIndex=0;
@@ -32,7 +33,12 @@ let acceptReg = function(handle, order, grid, ships, fleet, player){
 	playerRoster = {
 		[handle]: {grid: fleet.buildNauticalChart}
 	}
-	let pg = document.getElementById('playerGrid');
+	let pg = document.getElementById('playerGrid').appendChild(document.createElement('div'));;
+	
+	//let pgd = pg.appendChild(document.createElement('div'));
+	pg.id=handle;
+	pg.innerHTML=handle;
+
 	pg.appendChild(grid.clickableGrid(10, 10, ships, fleet, player, handle));
 }
 
@@ -49,9 +55,6 @@ let currentPlayer = function(){
 	return playerOrder[orderIndex];
 }
 
-let playerMove = function(){
-}
-
 let gameFlow = function(){
 	if (currentFlow != undefined){
 		document.getElementById(flow[currentFlow]).style.display='none';
@@ -62,14 +65,97 @@ let gameFlow = function(){
 	document.getElementById(flow[currentFlow]).style.display='inline';
 }
 
+let setPlayerMove = function(move){
+	let moveString;
+	if(playerMoveMap[move.coordinate] == undefined) {
+		playerMoveMap[move.coordinate] = playerMove.length;
+
+		if (move.type == 'attack') {
+			moveString = move.type + ': ' + move.coordinate;
+		}
+
+		let b = playerMoveBlock(move.coordinate, moveString);
+		playerMove.push(b);
+		document.getElementById('playOrder').appendChild(b);
+	}
+}
+
+let deletePlayerMove = function(){
+}
+
 let playerCanMove = function() {
-	if (playerOrder.length - 1 > playerMove.length) return true;
+	if (playerOrder.length > playerMove.length) return true;
 
 	return false;
 }
 
 let playerClearMove = function() {
 	player = [];
+}
+
+// Create a block to visually represent a move so it can be reordered if wanted
+let playerMoveBlock = function(handle, moveText) {
+	let b = document.createElement('div');
+	b.id = handle;
+	b.width = 100;
+	b.height = 21;
+
+	b.innerHTML=moveText;
+
+        b.setAttribute('draggable','true');
+	playerOrderHandler(b);
+	return b;
+}
+
+// Set up drag drop functionality for setting move order
+let playerOrderHandler = function(po) {
+    //document.getElementById('playOrder').setAttribute('draggable','true');
+    //player.playerOrderHandlers();
+    //let po = document.getElementById('playOrder');
+    po.addEventListener('dragstart',(function(e){
+	    e.dataTransfer.effectAllowed='move';
+	    e.dataTransfer.setData("text/plain",
+		JSON.stringify({
+			changeMove: e.target.id
+		})
+	    );
+    }));
+    po.addEventListener('dragover',(function(e){
+                    e.preventDefault();
+                    e.dataTransfer.dropEffect='move';
+                    return false;
+    }));
+    po.addEventListener('drop',(function(e){
+                    e.stopPropagation();
+                    e.preventDefault();
+                    let dropObj = JSON.parse(e.dataTransfer.getData("text/plain"));
+	    	    alterMoveIndex(dropObj.changeMove, e.target.id);
+	    	    displayMoveOrder();
+                    return false;
+    }));
+}
+
+function alterMoveIndex(startIndex, endIndex){
+	startId = startIndex;
+	startIndex = parseInt(playerMoveMap[startIndex]);
+	endIndex   = parseInt(playerMoveMap[endIndex]);
+
+	let begin = startIndex < endIndex ? parseInt(startIndex, 10) : parseInt(endIndex, 10);
+	let end =   startIndex < endIndex ? parseInt(endIndex, 10) : parseInt(startIndex, 10);
+	let hold = playerMove[startIndex];
+
+	while(begin < end){
+		document.getElementById(playerMove[begin].id).appendChild((playerMove[begin+1]));
+		playerMove[begin] = playerMove[begin+1];
+		playerMoveMap[startId] = begin+1;
+		begin++;
+	}
+	document.getElementById(playerMove[end].id).appendChild(document.getElementById[hold].id);
+	playerMove[end] = hold;
+	playerMoveMap[startId] = end;
+}
+
+function displayMoveOrder(){
 }
 
 module.exports = {
@@ -80,5 +166,9 @@ module.exports = {
     nextPlayer: nextPlayer,
     gameFlow: gameFlow,
     playerCanMove: playerCanMove,
-    playerClearMove: playerClearMove 
+    playerClearMove: playerClearMove,
+    setPlayerMove: setPlayerMove,
+    deletePlayerMove: deletePlayerMove,
+    playerOrderHandler: playerOrderHandler
+    //displayMoveOrder: displayMoveOrder;
 }
